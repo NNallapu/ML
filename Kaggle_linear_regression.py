b559d6542a2304ec6aade3fcebf468f9ad1cafe8
NNallapu <nandana221@gmail.com>
2018-02-17 21:18:19 -0600

    Add files via upload
    Using Kaggle's Simple Linear Regression dataset

---
+++ Kaggle_linear_regression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 17 17:31:40 2018
+
+@author: mittu
+"""
+# import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+train_df = pd.read_csv('train.csv')
